0e24bf0ad82256ce21f17ad4f78130cd9dc61dca
zoome-byte <karthik25shinu@gmail.com>
2025-07-08 16:38:07 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
