94d9cd8356dc5ce9f7877181502545b235b391d4
zoome-byte <karthik25shinu@gmail.com>
2025-07-08 16:35:19 +0530

    Create Skills.js

---
+++ Skills.js
@@ -0,0 +1,16 @@
+function Skills() {
+  return (
+    <section id="skills" className="section">
+      <h2>Skills</h2>
+      <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>React.js, Node.js</li>
+        <li>Linux & Terminal</li>
+        <li>Networking Basics</li>
+        <li>Ethical Hacking Fundamentals</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
