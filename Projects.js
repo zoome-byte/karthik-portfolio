6cdf8b226d0d0ebdd986d29ebeb2ed04e857814e
zoome-byte <karthik25shinu@gmail.com>
2025-07-08 16:36:00 +0530

    Create Projects.js

---
+++ Projects.js
@@ -0,0 +1,14 @@
+function Projects() {
+  return (
+    <section id="projects" className="section">
+      <h2>Projects</h2>
+      <div className="project">
+        <h3>Silly Story Generator</h3>
+        <p>A fun JS app that generates random stories. Built using HTML, CSS, JS.</p>
+        <a href="https://github.com/your-username/silly-story-generator" target="_blank">GitHub</a>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
