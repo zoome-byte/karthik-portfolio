e94a168bad10757a18014d8396f2aafec7a578cf
zoome-byte <karthik25shinu@gmail.com>
2025-07-08 16:30:31 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Karthik S Kumar</h1>
+      <p>Cybersecurity Enthusiast | Web Developer | Ethical Hacker</p>
+    </header>
+  );
+}
+
+export default Header;
