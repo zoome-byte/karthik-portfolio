a6f4343ee0296e381e96cd39e53508d97273a3c5
zoome-byte <karthik25shinu@gmail.com>
2025-07-08 16:30:59 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <section id="about" className="section">
+      <h2>About Me</h2>
+      <p>
+        I'm Karthik S Kumar, a 3rd-year B.Tech student at Trinity College of Engineering, 
+        specializing in Cybersecurity. I love ethical hacking, building cool projects, and making the internet a safer place!
+      </p>
+    </section>
+  );
+}
+
+export default About;
